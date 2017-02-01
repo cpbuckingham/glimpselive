45d0a5d04f67dcef18e074919a087d15bcac62bf
cam <cameron.p.buckingham@gmail.com>
2017-02-01 10:25:10 -0600

    updated drop table perams:

--- migrations/20170129172746_buddies.js
+++ migrations/20170129172746_buddies.js
@@ -9,5 +9,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('posts');
+    return knex.schema.dropTableIfExists('buddies');
 };
