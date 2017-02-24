ecf31162d59d9bc81f0bf5840ebd7d32e66c8b03
cpbuckingham <cameron.p.buckingham@gmail.com>
2017-02-24 12:21:29 -0600

    bagdes added with appropriate message amount on auth page

--- routes/auth.js
+++ routes/auth.js
@@ -26,6 +26,7 @@ function authorizedAdmin(req, res, next) {
 
 router.get('/', authorizedUser,  function (req, res) {
   let userID = req.session.user.id;
+  knex.from('users').innerJoin('messages', 'users.id', 'messages.sender_id').where('messages.user_id', userID).then(function (messages) {
   knex('users').where('id', userID).first().then(function (user){
     knex('users').innerJoin('posts', 'users.id', 'posts.user_id').then(function(posts) {
       knex('posts').where('user_id', userID).then(function (my_posts){
@@ -37,6 +38,8 @@ router.get('/', authorizedUser,  function (req, res) {
             my_posts: my_posts,
             comments: comments,
             buddies: buddies,
+            messages: messages
+              })
             })
           })
         })
