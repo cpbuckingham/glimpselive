4b506c3bc039aa717200a8eb8167dc2b5d6951b8
cpbuckingham <cameron.p.buckingham@gmail.com>
2017-02-28 10:45:01 -0600

    seperated out old messages and new message

--- migrations/20170129172741_messages.js
+++ migrations/20170129172741_messages.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       table.string('note').notNullable().defaultTo("");
       table.integer('sender_id').notNullable();
       table.integer('user_id').unsigned().index().references('id').inTable('users').onDelete('CASCADE');
+      table.boolean('read').defaultTo(false);
       table.timestamps(true, true);
     })
 };
