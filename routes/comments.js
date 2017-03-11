f7cd39397f6c20137071e946b4bfcb84ae4176f7
cpbuckingham <cameron.p.buckingham@gmail.com>
2017-03-11 10:54:09 -0600

    more work done on comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,22 @@ const router = express.Router();
 const knex = require('../db/knex');
 const bcrypt = require('bcrypt');
 
+router.get('/:id', function (req, res, next) {
+  let commentID = req.params.id;
+  knex('comments').where('id', commentID).first().then(function(comment){
+    res.render('comments/single', {
+      comment: comment,
+    })
+    console.log(comment);
+  })
+})
+
+router.get('/:id/edit', function (req, res, next) {
+  let postID = req.params.id;
+  knex('comments').where('post_id', postID).first().then(function(comment) {
+    res.render('comments/edit', {comment: comment})
+  })
+})
 
 router.put('/:id',function (req, res, next) {
   let commentID = req.params.id;
